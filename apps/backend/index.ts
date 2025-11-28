a1ecb7a084632e39d9c3557d072bf651c6dd8e98
Abhishek <abhishekvenky010@gmail.com>
2025-11-28 20:03:04 +0530

    put common add some backend

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,14 +1,50 @@
 import express from 'express';
 import cors from 'cors'
 import {prismaClient} from "db/client"
+import { authMiddleware } from './middleware';
+import jwt from 'jsonwebtoken';
+import {SignupSchema} from "common/inputs"
+import { config } from 'dotenv';
+config();
 const app = express()
-app.post("/signin",(req,res)=>{
+app.use(express.json());
+app.use(cors());
+app.post("/signin",async (req,res)=>{
+    const {success,data} = SignupSchema.safeParse(req.body);
+    if(!success){
+        res.status(403).json({
+            message:"incorrect credentials"
+        })
+        return
+    }
+    const email = data.email;
+    const password = data.password;
 
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email
+        }
+    });
+    if(!user){
+        res.status(403).json({
+            message : "user not found"
+        })
+        return;
+    }
+    if(user.password !== password){
+           res.status(403).json({
+            message:"incorrect credentials"
+        })
+        return
+    }
+    const token = jwt.sign({
+        userId : user.id
+    },process.env.JWT_SECRET as string)
+    res.json({ token });
 })
 app.get("/calendar",authMiddleware,(req,res)=>{
     
 })
-app.use(cors());
 app.listen(process.env.PORT||3000, () => {
     console.log('Server is running on port 3000')
 })
--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -1,5 +1,14 @@
-import type { NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 export const authMiddleware = (req:Request,res:Response,next:NextFunction)=>{
-    
+    const header = req.headers.authorization;
+    const token = header?.split(" ")?.[1];
+
+    if(!token){
+        res.status(403).json({
+            message : "missing token"
+        })
+    }
+    const {userId} = jwt.verify(token,process.env.JWT_SECRET) 
 }
